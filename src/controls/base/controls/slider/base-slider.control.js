ebada51261abd7dcedb27995e1f36502718a584d
Miriam Zusin <1rosehip@gmail.com>
2018-05-15 12:53:27 +0300

    range slider: fixed stop event

--- src/controls/base/controls/slider/base-slider.control.js
+++ src/controls/base/controls/slider/base-slider.control.js
@@ -72,6 +72,8 @@ class BaseSliderControl{
 
             document.addEventListener('mouseup', this.stop.bind(this));
             document.addEventListener('touchend', this.stop.bind(this));
+            window.addEventListener('mouseup', this.stop.bind(this));
+            window.addEventListener('touchend', this.stop.bind(this));
 
             this.element.addEventListener('mousedown', this.jump.bind(this));
 
